0aa196cdabdbf417d7f07a66801c68dfbc88f732
Pedro Henrique <pedhenriquecm@gmail.com>
2026-09-14 15:50:18 -0300

    fix: ajustar navegacao da home

--- src/main/java/br/com/mvc/controller/HomeServlet.java
+++ src/main/java/br/com/mvc/controller/HomeServlet.java
@@ -1,8 +1,18 @@
 package br.com.mvc.controller;
 
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/home")
 public class HomeServlet extends BaseServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/jsp/home.jsp").forward(request, response);
+    }
 }
